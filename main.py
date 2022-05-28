df5839ff50d94bfda0d0f2045ddec9ca5137a3cd
Stanley Ibe <ugoostanleyibe@gmail.com>
2022-05-28 23:25:34 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    result = sorted(word) == sorted(anagram)
+    return result
